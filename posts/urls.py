d4591387eb84cdeae5607329b662b11fb792abdc
alejo <alejo@micorreo.com>
2024-10-17 18:17:14 -0400

    post detail & new post link

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostCreateView
+from .views import PostListView, PostCreateView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
     path("post/new/", PostCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"), #<int:pk> pasamos un entero, que hace mencion al primary key, que es el indice del registro de la bace de datos
 ]
